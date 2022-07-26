b6b76b1e90be65a4a01e0f20eb419cb7389f13a5
daahunii <wjdekgns35@naver.com>
2022-07-26 17:19:49 +0900

    update Level A

--- Level A/A056.cpp
+++ Level A/A056.cpp
@@ -30,12 +30,39 @@ using namespace std;
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
+        vector<int> result;
+        for(int i=0; i<nums1.size(); i++){
+            for(int j=0; j<nums2.size(); j++){
+                if(nums1[i] == nums2[j]){
+                    if(j+1 >= nums2.size()){ // 배열 사이즈를 초과해서 다음 큰 수가 없는 경우
+                        result.push_back(-1);
+                        break;
+                    }
+                    int nextchk = 0; // 더 큰 숫자가 있는지 체크용 
+                    for(int k=j+1; k<nums2.size(); k++){ // 다음 숫자를 하나하나 검사해 찾아서
+                        if(nums2[j] < nums2[k]){
+                            result.push_back(nums2[k]); // 찾으면 수를 넣고 체크
+                            nextchk = 1;
+                            break;
+                        }
+                        else nextchk = 0;
+                    }
+                    if(nextchk == 0){   // 못 찾은 경우 -1 추가
+                        result.push_back(-1);
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
     }
 };
 
 int main()
 {
-
+    vector<int> n1 = {1,3,5,2,4};
+    vector<int> n2 = {6,5,4,3,2,1,7};
+    vector<int> result = Solution().nextGreaterElement(n1, n2);
+    for(int i=0; i<result.size(); i++) cout << result[i] << endl;
     return 0;
 }
---
+++ Level A/A058.cpp
@@ -0,0 +1,74 @@
+/*
+Leet Code
+<206. Reverse Linked List>
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+=======================================================================================
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+=======================================================================================
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *newHead = NULL;
+        while(head != NULL){
+            ListNode *next = head->next;
+            head->next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+};
+
+int main()
+{
+    ListNode* node1 = new ListNode(1);
+    ListNode* node2 = new ListNode(2);
+    ListNode* node3 = new ListNode(3);
+    ListNode* node4 = new ListNode(4);
+    ListNode* node5 = new ListNode(5);
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = NULL;
+
+    ListNode *result = Solution().reverseList(node1);
+    while(result != NULL){
+        cout << result->val << endl;
+        result = result->next;
+    }
+    return 0;
+}
