8a5e05f01015c5e1439b582d651bb0b035c7d785
daahunii <wjdekgns35@naver.com>
2022-07-27 15:18:15 +0900

    update Level A

---
+++ Level A/A060.cpp
@@ -0,0 +1,67 @@
+/*
+Leet Code
+<682. Baseball Game>
+
+You are keeping score for a baseball game with strange rules.
+The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+At the beginning of the game, you start with an empty record.
+You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record
+and is one of the following:
+
+- An integer x - Record a new score of x.
+- "+" - Record a new score that is the sum of the previous two scores.
+It is guaranteed there will always be two previous scores.
+- "D" - Record a new score that is double the previous score.
+It is guaranteed there will always be a previous score.
+- "C" - Invalidate the previous score, removing it from the record.
+It is guaranteed there will always be a previous score.
+
+Return the sum of all the scores on the record.
+The test cases are generated so that the answer fits in a 32-bit integer.
+=======================================================================================================
+Example 1:
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+
+Example 2:
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+
+Example 3:
+Input: ops = ["1","C"]
+Output: 0
+=======================================================================================================
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        int result = 0;
+        vector<int> v;
+        for(int i=0; i<ops.size(); i++){
+            if(ops[i] >= "0" && ops[i] <= "9") v.push_back(stoi(ops[i]));
+            else if(ops[i] == "C") v.pop_back();
+            else if(ops[i] == "D") v.push_back(v[v.size()-1]*2);
+            else if(ops[i] == "+") v.push_back(v[v.size()-2]+v[v.size()-1]);
+            else v.push_back(stoi(ops[i])); // 음수이거나, 십의자리, 백의자리가 넘어가는 수들
+        }
+        for(int j=0; j<v.size(); j++){
+            result += v[j];
+            // cout << v[j] << " ";
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    vector<string> ops = {"63","D","-53","95","85","-89","+","61","6","-12"};
+    int result = Solution().calPoints(ops);
+    cout << result;
+    return 0;
+}
